7cd050fef67ccfcac983bd6ab3929fb9288308a9
TheParadox20 <onyambusamson@gmail.com>
2023-12-27 00:40:37 +0300

    create test route

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def read_root():
 
 @app.get('/test')
 def test():
-    return {'test': 'test'}
+    return "Fuck society"
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
